799146ec31bc61c6d13564a9ace095527b8d044b
marxattax <cuisy.vincent@gmail.com>
2024-05-20 00:21:42 +0200

    Code réorganisé et ajout de styles pour affichage galerie et boutons filtres

--- FrontEnd/affichageWorks.js
+++ FrontEnd/affichageWorks.js
@@ -1,68 +1,60 @@
-const cateFetch = fetch(urlCate).then(resp => resp.json())
-const worksFetch = fetch(urlWorks).then(resp => resp.json())
-Promise.all([cateFetch, worksFetch])
-.then(([dataCate, dataWorks]) => {
-
 /* Creation d'une fonction pour afficher les projets */
-function affichageWorks(works) {
-    for(i=0; i < works.length ; i++) {
+function affichageWorks(target) {
+    for(i=0; i < target.length ; i++) {
         const figureWorks = document.querySelector("#works")
-        const worksElement = document.createElement("figure")
+        const work = document.createElement("figure")
+        work.className = "figWork"
 
-        const imageElement = document.createElement("img")
-        imageElement.src = works[i].imageUrl
-        imageElement.alt = works[i].title
+        const image = document.createElement("img")
+        image.src = target[i].imageUrl
+        image.alt = target[i].title
 
-        const titleElement = document.createElement("figcaption")
-        titleElement.innerHTML = works[i].title
+        const title = document.createElement("figcaption")
+        title.innerHTML = target[i].title
 
-        figureWorks.appendChild(worksElement)
-        worksElement.appendChild(imageElement)
-        worksElement.appendChild(titleElement)
+        figureWorks.appendChild(work)
+        work.appendChild(image)
+        work.appendChild(title)
     }
 }
 
-affichageWorks(dataWorks)
+affichageWorks(works)
 
-/* Construction d'un bouton sans filtre */
+/* Bouton affichage sans filtre */
 const divFiltres = document.querySelector("#filtres")
 const nofiltre = document.createElement("button")
-nofiltre.id = "tous"
 nofiltre.innerText = "Tous"
+nofiltre.id ="tous"
+nofiltre.classList = "button-filtre button-active"
 divFiltres.appendChild(nofiltre)
-
-/* Ajout du clic sur le bouton "tous" */
 const btnNofiltre = document.querySelector("#tous")
 btnNofiltre.addEventListener("click", function() {
+    const activeButton = document.querySelector(".button-active")
+    activeButton.classList.remove("button-active")
+    nofiltre.classList.add("button-active")
     document.querySelector("#works").innerHTML = ""
-    affichageWorks(dataWorks)
+    affichageWorks(works)
 })
 
-/* Construction de boutons pour filtrer en fonction de la categorie du projet */
-for(var i=0; i < dataCate.length; i++) {
-    const cate = dataCate
-    const divFiltres = document.querySelector("#filtres")
-    const filtreElement = document.createElement("button")
-    filtreElement.innerText = cate[i].name
-    filtreElement.id = "cate" + cate[i].id
-    
-    divFiltres.appendChild(filtreElement)
-}
-        
-/* Filtre des projets en fonction de la categorie, puis affichage */
-for(var i=0; i < dataCate.length; i++) {
-    const idCate = dataCate[i].id
-    const boutonFiltre = document.querySelectorAll("#cate" + idCate)
-    boutonFiltre.forEach(filtre => {
-        filtre.addEventListener("click", function() {
-            const filtrage = dataWorks.filter(function(categorie) {
-                return categorie.category.id == idCate
-            })
-            document.querySelector("#works").innerHTML = ""
-            affichageWorks(filtrage)
+/* Boutons pour filtrer en fonction de la categorie du projet */
+for(var i=0; i < categorie.length; i++) {
+    const cate = categorie[i]
+    const filtre = document.createElement("button")
+    filtre.className = "button-filtre"
+    filtre.innerText = cate.name
+    filtre.id = "cate" + cate.id
+
+    filtre.addEventListener("click", function() {
+        const filtrage = works.filter(function(work) {
+            return work.category.id == cate.id
         })
+        const activeButton = document.querySelector(".button-active")
+        activeButton.classList.remove("button-active")
+        filtre.classList.add("button-active")
+        document.querySelector("#works").innerHTML = ""
+        affichageWorks(filtrage)
     })
+    divFiltres.appendChild(filtre)
 }
-        
-})
+
 
